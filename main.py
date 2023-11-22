24f390b21157691ae177d65f292b227d65496d58
Oluwatobi Ojebiyi <tobiojebiyi@gmail.com>
2023-11-22 01:07:23 +0000

    rewrite main.py to original code delete old note book file

--- main.py
+++ main.py
@@ -2,58 +2,45 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+# Load Movie Data
+movies = pd.read_csv("movies_metadata.csv",
+                     usecols=["id", "overview", "title", "vote_average", "vote_count", "release_date"])
+movies = movies.dropna().drop_duplicates().reset_index(drop=True)
+movies = movies.rename(columns={"id": "movieId"})
+movies["movieId"] = movies["movieId"].astype("int64")
 
-def load_and_preprocess_data():
-    # Load Movie Data
-    movies = pd.read_csv("movies_metadata.csv",
-                         usecols=["id", "overview", "title", "vote_average", "vote_count", "release_date"])
-    movies = movies.dropna().drop_duplicates().reset_index(drop=True)
-    movies = movies.rename(columns={"id": "movieId"})
-    movies["movieId"] = movies["movieId"].astype("int64")
+# Load Ratings Data
+ratings = pd.read_csv("ratings_small.csv")
+ratings["date"] = pd.to_datetime(ratings["timestamp"], unit="s")
+ratings = ratings.drop("timestamp", axis=1)
 
-    # Load Ratings Data
-    ratings = pd.read_csv("ratings_small.csv")
-    ratings["date"] = pd.to_datetime(ratings["timestamp"], unit="s")
-    ratings = ratings.drop("timestamp", axis=1)
+# Merge Movie and Ratings Data
+movie_ratings = pd.merge(ratings, movies, on="movieId")
 
-    # Merge Movie and Ratings Data
-    movie_ratings = pd.merge(ratings, movies, on="movieId")
+# Create a pivot table with user ratings
+user_movie_ratings = movie_ratings.pivot_table(index='userId', columns='title', values='rating')
 
-    return movie_ratings, movies
+# Fill missing values with 0 (assuming missing values mean the user hasn't rated that movie)
+user_movie_ratings = user_movie_ratings.fillna(0)
 
+# Feature extraction using TF-IDF on movie overviews
+tfidf_vectorizer = TfidfVectorizer(stop_words='english', min_df=5)
+tfidf_matrix = tfidf_vectorizer.fit_transform(movies['overview'])
 
-def create_user_movie_ratings_pivot(movie_ratings):
-    # Create a pivot table with user ratings
-    user_movie_ratings = movie_ratings.pivot_table(index='userId', columns='title', values='rating')
-
-    # Fill missing values with 0 (assuming missing values mean the user hasn't rated that movie)
-    user_movie_ratings = user_movie_ratings.fillna(0)
-
-    return user_movie_ratings
-
-
-def calculate_cosine_similarity(movies):
-    # Feature extraction using TF-IDF on movie overviews
-    tfidf_vectorizer = TfidfVectorizer(stop_words='english', min_df=5)
-    tfidf_matrix = tfidf_vectorizer.fit_transform(movies['overview'])
-
+# Compute similarity scores
+try:
     # Compute similarity scores
-    try:
-        cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
-    except Exception as e:
-        raise RuntimeError(f"Error computing similarity scores: {e}")
-
-    return cosine_sim
+    cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
+except Exception as e:
+    print(f"Error computing similarity scores: {e}")
 
 
-def get_content_based_recommendations(title, cosine_sim, user_ratings, movies):
-    # Input validation
-    matching_indices = movies.index[movies['title'].str.lower() == title.lower()].tolist()
-    if not matching_indices:
+# Function to get movie recommendations
+def get_content_based_recommendations(title, cosine_sim=cosine_sim, user_ratings=user_movie_ratings, movies=movies):
+    if title not in movies['title'].values:
         raise ValueError("Movie title not found in the dataset.")
-    elif len(matching_indices) > 1:
-        raise ValueError("Multiple movies with the same title found in the dataset. Provide a more specific title.")
-    idx = matching_indices[0]
+    # Get the index of the movie that matches the title
+    idx = movies.index[movies['title'].str.lower() == title.lower()].tolist()[0]
 
     # Get the pairwise similarity scores of all movies with that movie
     sim_scores = list(enumerate(cosine_sim[idx]))
@@ -74,22 +61,6 @@ def get_content_based_recommendations(title, cosine_sim, user_ratings, movies):
     return unrated_movies[:5]
 
 
-def main():
-    movie_ratings, movies = load_and_preprocess_data()
-    user_movie_ratings = create_user_movie_ratings_pivot(movie_ratings)
-    cosine_sim = calculate_cosine_similarity(movies)
-
-    # Take user input for the movie title
-    user_input_title = input("Enter the title of a movie you've watched: ")
-
-    try:
-        # Get content-based movie recommendations based on user preferences
-        recommended_movies = get_content_based_recommendations(user_input_title, cosine_sim, user_movie_ratings, movies)
-        print("Recommended movies:")
-        print(recommended_movies)
-    except ValueError as ve:
-        print(f"Error: {ve}")
-
-
-if __name__ == "__main__":
-    main()
+# Get content-based movie recommendations based on user preferences
+recommended_movies = get_content_based_recommendations('The God father')
+print(recommended_movies)
